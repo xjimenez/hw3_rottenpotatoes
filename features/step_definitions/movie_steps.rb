2683ed65a063f36a6fa26ad1b25d015ade05f5cd
Javier Jimenez <xjimenez@gmail.com>
2012-06-15 04:16:40 -0700

    hw3-part1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,11 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
+    ## Uncomment the following line for debugging purposes:
+    # debugger
   end
-  flunk "Unimplemented"
+  # flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
